4b4d81b90e8b8b805164d859a0a724bf13871521
fernandagouveia07 <fernanda.silva.1633637@sga.pucminas.br>
2026-04-22 23:54:55 -0300

    primeiro commit

---
+++ projeto-mf/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace projeto_mf.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
