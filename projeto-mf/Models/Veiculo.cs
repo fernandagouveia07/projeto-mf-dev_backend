bcc3281c4556ca932ec3a545928a1ceccc85a5e0
fernandagouveia07 <fernanda.silva.1633637@sga.pucminas.br>
2026-04-23 18:57:55 -0300

    Model Veiculo criado

---
+++ projeto-mf/Models/Veiculo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projeto_mf.Models
+{
+	[Table("Veiculos")]
+	public class Veiculo
+	{ 
+		[Key]
+		public int Id { get; set; }
+
+		[Required(ErrorMessage ="Informe seu nome")]
+		public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe sua placa")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Informe o ano de fabricação")]
+
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Informe o ano do modelo.")]
+
+        public int AnoModelo { get; set; }
+    }
+}
+
