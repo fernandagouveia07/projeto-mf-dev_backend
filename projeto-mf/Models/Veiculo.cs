7c030a3a69bd51e6bbbc9c8896a93164fdcf4d09
fernanda <1633637@sga.pucminas.br>
2026-04-24 21:44:01 -0300

    Add Create Veiculos

--- projeto-mf/Models/Veiculo.cs
+++ projeto-mf/Models/Veiculo.cs
@@ -10,17 +10,17 @@ namespace projeto_mf.Models
 		[Key]
 		public int Id { get; set; }
 
-		[Required(ErrorMessage ="Informe seu nome")]
+		[Required(ErrorMessage ="Informe o nome")]
 		public string Nome { get; set; }
 
-        [Required(ErrorMessage = "Informe sua placa")]
+        [Required(ErrorMessage = "Informe a placa")]
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Informe o ano de fabricação")]
         [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
 
-        [Required(ErrorMessage = "Informe o ano do modelo.")]
+        [Required(ErrorMessage = "Informe o ano do modelo")]
         [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
     }
